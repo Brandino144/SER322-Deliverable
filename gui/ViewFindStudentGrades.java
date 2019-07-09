2b5576889d4b7914594516bf0f30f248918278bb
Cristina Gonzales <cgonza59@asu.edu>
2019-07-08 19:25:15 -0700

    find student grades

--- gui/ViewFindStudentGrades.java
+++ gui/ViewFindStudentGrades.java
@@ -7,8 +7,11 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -19,7 +22,8 @@ public class ViewFindStudentGrades {
 
     private Connection conn;
     public JFrame frame;
-    private JTable table;
+    private JTextArea table;
+    private JComboBox comboBox;
 
     /**
      * Create the application.
@@ -92,18 +96,73 @@ public class ViewFindStudentGrades {
         btnSubmit.setBounds(382, 58, 117, 29);
         frame.getContentPane().add(btnSubmit);
         
-        table = new JTable();
+        btnSubmit.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) { 
+               submitButtonPressed();
+            } 
+          } );
+        
+        
+        table = new JTextArea();
         table.setBounds(49, 155, 784, 568);
         frame.getContentPane().add(table);
         
         ArrayList<String> studentIds = getStudentIds();
         
-        JComboBox comboBox = new JComboBox();
+        comboBox = new JComboBox();
         comboBox.setBounds(242, 71, 125, 27);
         frame.getContentPane().add(comboBox);
         for (int i = 0; i < studentIds.size(); i++) {
             comboBox.addItem(studentIds.get(i));
         }
+        
+        
+        
+        
     }
+    
+    public void GetGrades(String asuid) {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        if (conn == null) {
+            return;
+        }
+        try {
+            stmt = conn.prepareStatement("SELECT * FROM GRADES WHERE `ASU ID` = ?");
+            stmt.setString(1, asuid);
+            rs = stmt.executeQuery();
+            
+            table.setText("Course ID\tSection\tGrade\n");
+            while (rs.next()) {
+                String newline = rs.getString(3) + "\t" 
+                                + rs.getString(4) + "\t" 
+                                + rs.getString(5) + "\n";
+                table.setText(table.getText()+newline);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Sql Error");
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException e) {
+                System.out.println("Error closing sql statement");
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    
+    public void submitButtonPressed() {
+        GetGrades((String) comboBox.getSelectedItem());
+        
+        
+    }
+    
+
 
 }
