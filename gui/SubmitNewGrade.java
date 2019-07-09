7a762dd44acd4eaac659ea86a0329bc986b35d14
Jesse Quy <jqhieu@gmail.com>
2019-07-08 19:38:22 -0700

    second attempt

--- gui/SubmitNewGrade.java
+++ gui/SubmitNewGrade.java
@@ -8,6 +8,10 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.swing.JComboBox;
 import javax.swing.JButton;
@@ -57,10 +61,21 @@ public class SubmitNewGrade {
         JComboBox comboBox = new JComboBox();
         comboBox.setBounds(242, 71, 125, 27);
         frame.getContentPane().add(comboBox);
+        ArrayList<String> ASUIDs = fillComboBox1();
+        
+        for(int i = 0; i < ASUIDs.size(); i++) {
+            comboBox.addItem(ASUIDs.get(i));
+        }
         
         JComboBox comboBox_1 = new JComboBox();
         comboBox_1.setBounds(242, 137, 171, 27);
         frame.getContentPane().add(comboBox_1);
+        
+        ArrayList<String> CourseIDs = fillComboBox1();
+        
+        for(int i = 0; i < CourseIDs.size(); i++) {
+            comboBox_1.addItem(CourseIDs.get(i));
+        }
         
         JButton btnSubmit = new JButton("Submit");
         btnSubmit.addActionListener(new ActionListener() {
@@ -89,8 +104,85 @@ public class SubmitNewGrade {
         
     }
     
+    public ArrayList<String> fillComboBox1() {
+        
+        Statement stmt = null;
+        ResultSet rs = null;
+        
+        ArrayList<String> ids = new ArrayList<String>();
+        
+        try {
+            //String query = "SELECT * FROM GRADES";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM GRADES");
+            
+            while (rs.next()) {
+                ids.add(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return ids;
+    }
+    
+    public ArrayList<String> fillComboBox2() {
+        Statement stmt = null;
+        ResultSet rs = null;
+        
+        ArrayList<String> ids = new ArrayList<String>();
+        
+        try {
+            //String query = "SELECT * FROM GRADES";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM GRADES");
+            
+            while (rs.next()) {
+                ids.add(rs.getString(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return ids;
+    }
+    
     // TODO
     public void submitButton() {
-        
+        /*try {
+            Class.forName(Driver);
+            Connection conn = DriverManager.getConnection();
+            String query = "UPDATE GRADES SET GRADE =" + txtEnterGpa.getText() + "WHERE ASU ID = " + "Course ID = " + ;
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, );
+            stmt.setString(2, );
+            stmt.setString(3, txtEnterGpa.getText());
+            stmt.execute();
+            JOptionPane.showMessageDialog(null, "New grade submitted!");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }*/
     }
 }
+
